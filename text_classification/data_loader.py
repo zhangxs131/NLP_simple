acdd7564b0895b67a40cf1cb38fb57124d190b52
zhangxs131 <96735521+zhangxs131@users.noreply.github.com>
2022-10-14 11:40:35 +0800

    Create data_loader.py

---
+++ text_classification/data_loader.py
@@ -0,0 +1,68 @@
+from transformers import AutoTokenizer
+import torch
+import os
+from torch.utils.data import Dataset,DataLoader
+
+class SequenceClassificationDataset(Dataset):
+
+    def __init__(self,data_path,mode='train'):
+        super().__init__()
+        self.mode=mode
+        self.data=self._load_file(data_path)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self,i):
+        return self.data[i]
+
+    def _load_file(self,file_name):
+
+        with open(file_name,'r',encoding='utf-8') as f:
+            content=f.readlines()
+        data=[]
+        if self.mode!='test':
+            for i in content:
+                t=i.strip('\n')
+                t=t.split('\t')
+                data.append(
+                    {'text':t[0],
+                     'label':int(t[1]) 
+                    } 
+                )#这里label已经换成数字，如果需要str转换，需要简单修改。)
+        else:
+            for i in content:
+                t=i.strip('\n')
+                data.append({'text':t})   
+            
+        return data
+
+#collate_fn=lambda x: collate_fn(x, info)
+def collate_fn(batch,tokenizer):
+    input=tokenizer([i['text'] for i in batch],padding=True,truncation=True,max_length=512,return_tensors='pt')
+    input['labels']=torch.LongTensor([i['label'] for i in batch])
+
+    return input
+
+def collate_fn_test(batch,tokenizer):
+    input=tokenizer([i['text'] for i in batch],padding=True,truncation=True,max_length=512,return_tensors='pt')
+
+    return input
+
+
+
+def main():
+    train_path='data/train.txt'
+    pretrained_path='../pretrained_model/bert-base-chinese'
+
+
+    train_dataset=SequenceClassificationDataset(train_path)
+    tokenizer=AutoTokenizer.from_pretrained(pretrained_path)
+    dataloader=DataLoader(train_dataset,batch_size=32,shuffle=True,collate_fn=lambda x:collate_fn(x,tokenizer))
+    for i in dataloader:
+        print(i['input_ids'].shape)
+        break
+   
+
+if __name__=='__main__':
+    main()
